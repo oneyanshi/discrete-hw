fa171219da36b7224ad05233bdcf32b19a9714ae
Yan Shi <ys7615a@student.american.edu>
2016-10-09 18:48:50 -0400

    Update files

--- Shi_Yan_Homework4_Q5.py
+++ Shi_Yan_Homework4_Q5.py
@@ -7,4 +7,28 @@ PROMPT:
     Write a program which prints out all rearrangements of a word, with no repeats.
     Assume that a word consists of capital English letters
 
+    Example:
+
+    ABC
+    Permutations:
+    ABC
+    ACB
+    BAC
+    BCA
+    CAB
+    CBA
+
 '''
+def permutation(word):
+    letter = [word[0]] #first letter into the list!
+    for i in range(1, len(word)): #traversing through the word
+        return_list = [] #create the return list
+        for item in letter:
+            return_list.append(item+word[i])
+            for j in range(len(item)):
+                return_list.append(item[0:j] + word[i] + item[j:])
+        letter = return_list
+    return return_list
+
+word = raw_input("Input a word that you'd like to see the permutations of: ").lower()
+print permutation(word)
