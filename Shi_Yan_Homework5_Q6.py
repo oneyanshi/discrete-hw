027123a72cb6b64dbd85dec04bfb3c420987e2c9
Yan Shi <ys7615a@student.american.edu>
2016-11-13 02:00:56 -0500

    Update homework

--- Shi_Yan_Homework5_Q6.py
+++ Shi_Yan_Homework5_Q6.py
@@ -9,4 +9,32 @@ DATE: November 6, 2017
   will start getting big fast, so you'll want to "mod out" by n after
   each new multiplication by a.
 
+  answer = a^k mod n
+
 '''
+
+def raisePowerMod(a, k, n):
+	answer = 1
+	while 1:
+		if k % 2 == 1:
+			answer = answer * a % n
+		k /=2
+		if k == 0:
+			break
+		a = a * a % n
+
+	return answer
+
+def main():
+	print "This finds a^k mod n."
+	a, k, n = input("a: "), input("k: "), input("n: ")
+	print "The answer is: " + str(raisePowerMod(a, k, n))
+	user_input = input("Would you like to try again? Y = 1 | N = 0 \n" )
+	if user_input == 1:
+		main()
+	elif user_input == 0:
+		return 
+	else:
+		user_input = "Invalid. Y = 1 | N = 0 \n"
+
+main()
